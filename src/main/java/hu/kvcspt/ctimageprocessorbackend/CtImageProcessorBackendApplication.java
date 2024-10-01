2d41f2f74691cee92774d0087ee298f7053ca689
Patricia <kvcspt0@gmail.com>
2024-10-01 18:05:53 +0200

    Initial commit

---
+++ src/main/java/hu/kvcspt/ctimageprocessorbackend/CtImageProcessorBackendApplication.java
@@ -0,0 +1,13 @@
+package hu.kvcspt.ctimageprocessorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtImageProcessorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtImageProcessorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/kvcspt/ctimageprocessorbackend/CtImageProcessorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.kvcspt.ctimageprocessorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtImageProcessorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
