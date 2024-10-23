ee10677fae0e71b53688da9edaf36eda1599f914
Patricia <kvcspt0@gmail.com>
2024-10-23 10:58:57 +0200

    feat: add Builder to SectionTemplate model

--- src/main/java/hu/kvcspt/ctreportingtoolbackend/model/SectionTemplate.java
+++ src/main/java/hu/kvcspt/ctreportingtoolbackend/model/SectionTemplate.java
@@ -2,6 +2,7 @@ package hu.kvcspt.ctreportingtoolbackend.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Entity
 @Table(name = "section_templates")
 public class SectionTemplate {
