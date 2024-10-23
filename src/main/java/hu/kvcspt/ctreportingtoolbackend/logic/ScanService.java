5ae88b0113147097ec37c2213151237e421270fa
Patricia <kvcspt0@gmail.com>
2024-10-23 09:24:26 +0200

    feat: Scan service

---
+++ src/main/java/hu/kvcspt/ctreportingtoolbackend/logic/ScanService.java
@@ -0,0 +1,37 @@
+package hu.kvcspt.ctreportingtoolbackend.logic;
+
+import hu.kvcspt.ctreportingtoolbackend.model.Scan;
+import hu.kvcspt.ctreportingtoolbackend.model.repository.ScanRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@AllArgsConstructor
+@Log4j2
+public class ScanService {
+    private ScanRepository scanRepository;
+    public List<Scan> getAllScans(){
+        return scanRepository.findAll();
+    }
+    public Scan getScanById(Long id){
+        return scanRepository.getReferenceById(id);
+    }
+    public Scan updateScan(Scan scan){
+        if(scanRepository.existsById(scan.getId())){
+            return scanRepository.save(scan);
+        }
+        throw new IllegalArgumentException("Scan not found!");
+    }
+
+    public Scan createScan(Scan scan){
+        return scanRepository.save(scan);
+    }
+
+    public void deleteScan(Scan scan){
+        scanRepository.delete(scan);
+        log.debug("Scan is deleted successfully");
+    }
+}
