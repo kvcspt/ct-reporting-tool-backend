520c26b4a55b1e55d1763a14df11c892facc642a
Patricia <kvcspt0@gmail.com>
2024-10-23 10:57:02 +0200

    feat: create User controller

---
+++ src/main/java/hu/kvcspt/ctreportingtoolbackend/controller/UserController.java
@@ -0,0 +1,41 @@
+package hu.kvcspt.ctreportingtoolbackend.controller;
+
+import hu.kvcspt.ctreportingtoolbackend.logic.UserService;
+import hu.kvcspt.ctreportingtoolbackend.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public final class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        user.setId(id);
+        return userService.updateUser(user);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(User.builder().id(id).build());
+    }
+}
