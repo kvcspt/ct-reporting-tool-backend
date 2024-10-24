5f23782df6b1345da1f8898fcd0d3b6da88d2918
Patricia <kvcspt0@gmail.com>
2024-10-24 16:56:25 +0200

    feat: ReportTemplate DTO and changed service, controller

---
+++ src/main/java/hu/kvcspt/ctreportingtoolbackend/dto/ReportTemplateDTO.java
@@ -0,0 +1,21 @@
+package hu.kvcspt.ctreportingtoolbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReportTemplateDTO {
+    private Long id;
+    private String name;
+    private Map<String, String> sections = new HashMap<>();
+    private List<Long> reportIds;
+}
